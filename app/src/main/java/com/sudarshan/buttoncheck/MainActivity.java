a137b902d9e94f4111e2cdfbd442eba680771870
s.sinha2103@outlook.com <s.sinha2103@outlook.com>
2020-02-06 23:59:10 +0530

    Initial commit

---
+++ app/src/main/java/com/sudarshan/buttoncheck/MainActivity.java
@@ -0,0 +1,57 @@
+package com.sudarshan.buttoncheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+    Button button2;
+    TextView textview;
+    ConstraintLayout abcd;
+
+    public void press(View v)
+    {
+        button1.setEnabled(false);
+        button2.setEnabled(true);
+        textview.setText("Button 2 enabled");
+        abcd.setBackgroundColor(Color.DKGRAY);
+    }
+    public void press2(View v)
+    {
+        button1.setEnabled(true);
+        button2.setEnabled(false);
+        textview.setText("Button 1 enabled");
+        abcd.setBackgroundColor(Color.GRAY);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        textview = findViewById(R.id.textView);
+        abcd =  findViewById(R.id.abcd);
+
+        button1.setEnabled(true);
+        button2.setEnabled(false);
+        textview.setText("Button 1 enabled");
+        abcd.setBackgroundColor(Color.GRAY);
+
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                button1.setBackgroundColor(Color.BLACK);
+//            }
+//        });
+    }
+}
